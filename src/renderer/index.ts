d303bbbc783670b3e7796d4953235923460f0cc3
NovusTheory <novustheory@gmail.com>
2020-01-27 16:41:42 -0600

    Update to latest Vue

--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -1,4 +1,4 @@
-import { createRenderer, warn, App, RootRenderFunction } from '@vue/runtime-core';
+import { createRenderer, warn, App, RootRenderFunction, CreateAppFunction } from '@vue/runtime-core';
 import { nodeOps } from './nodeOps';
 import { patchProp } from './patchProp';
 import { VNNode, VNElement } from '../components';
@@ -11,12 +11,12 @@ const { render: baseRender, createApp: baseCreateApp } = createRenderer<VNNode, 
 
 export const render = baseRender as RootRenderFunction<VNNode, VNElement>;
 
-export const createApp = (): App<VNElement> => {
-    const app = baseCreateApp();
+export const createApp: CreateAppFunction<VNElement> = (...args) => {
+    const app = baseCreateApp(...args);
 
     const mount = app.mount;
-    app.mount = (component: any, rootContainer: Window, rootProps?: any | undefined): any => {
-        return mount(component, rootContainer, rootProps);
+    app.mount = (rootContainer: Window): any => {
+        return mount(rootContainer);
     }
 
     return app
